0d39ac6cae97f22c9f2993db3925301d94c3ec7d
dengkang <dengkang@mails.swust.edu.cn>
2022-04-03 20:29:38 +0800

    add example

---
+++ examples/echo/echo.cc
@@ -0,0 +1,28 @@
+#include "tohka/ioloop.h"
+#include "tohka/netaddress.h"
+#include "tohka/tcpserver.h"
+#include "tohka/util/log.h"
+
+using namespace tohka;
+
+void OnConnection(const TcpEventPrt_t& conn) {
+  if (conn->Connected()) {
+    log_info("new connection from: %s", conn->GetPeerIpAndPort().c_str());
+  } else {
+    log_info("connection Close: %s", conn->GetPeerIpAndPort().c_str());
+  }
+}
+
+void OnMessage(const TcpEventPrt_t& conn, IoBuf* buf) { conn->Send(buf); }
+
+int main() {
+  IoLoop* loop = IoLoop::GetLoop();
+  NetAddress address(6666);
+  log_info("server listen on:%s", address.GetIpAndPort().c_str());
+  TcpServer server(loop->GetWatcherRawPoint(), address);
+  server.SetOnConnection(OnConnection);
+  server.SetOnMessage(OnMessage);
+  server.Run();
+  loop->RunForever();
+  return 0;
+}
