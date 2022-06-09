7aec5ef0013b5144a87891a43a3a7047568f547a
dengkang <dengkang@mails.swust.edu.cn>
2022-06-09 18:02:50 +0800

    update and bug fixed

--- tohka/util/log.cc
+++ tohka/util/log.cc
@@ -119,12 +119,11 @@ static void init_event(log_Event* ev, void* udata) {
 }
 
 void log_log(int level, const char* file, int line, const char* fmt, ...) {
-  log_Event ev = {
-      .fmt = fmt,
-      .file = file,
-      .line = line,
-      .level = level,
-  };
+  log_Event ev;
+  ev.fmt = fmt;
+  ev.file = file;
+  ev.line = line;
+  ev.level = level;
 
   lock();
 
